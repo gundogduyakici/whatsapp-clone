46adf5e943f6765eaacc27749b8f364cc317d626
Gündoğdu Yakıcı <dodis@live.com>
2021-06-09 17:23:02 +0300

    Moved to Github

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './App.css';
+import Sidebar from './Sidebar';
+
+function App() {
+  return (
+    <div className="app">
+      <div className="app__body">
+        <Sidebar />
+        {/* Chat */}
+      </div>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyBR1AMvRRNHcJ4F2tziNz8d75WBZjnLjj8",
+    authDomain: "whatsapp-clone-8f835.firebaseapp.com",
+    projectId: "whatsapp-clone-8f835",
+    storageBucket: "whatsapp-clone-8f835.appspot.com",
+    messagingSenderId: "272013210573",
+    appId: "1:272013210573:web:29705e895c22ff6bdf8a84",
+    measurementId: "G-EJ4NEW7EEE"
+};
